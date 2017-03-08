fbba4f9c6064e33a7356ff9ff0851af54f38a37a
Jay Dunkelberger <ldunkelberger@pivotal.io>
2017-03-08 13:41:08 -0800

    Install ginkgo from vendored ginkgo in tests
    [#140988047]
    
    Signed-off-by: Joachim Valdez <valdezj@us.ibm.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,8 +5,6 @@ set -o pipefail
 
 cd $(dirname $0)/..
 
-go get github.com/tools/godep
-go get github.com/onsi/ginkgo
-go install github.com/onsi/ginkgo/ginkgo
+go install ./vendor/github.com/onsi/ginkgo/ginkgo
 
 ginkgo acceptance
