0dfe91f0c49994a60aca19f35415ec80898bfbfd
Gabe Rosenhouse <grosenhouse@pivotal.io>
2017-04-24 01:50:43 -0700

    Clean up test scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,62 +5,43 @@ set -o pipefail
 
 cd $(dirname $0)/..
 
-function bootPostgres {
-	echo -n "booting postgres"
-	(/docker-entrypoint.sh postgres &> /var/log/postgres-boot.log) &
-	trycount=0
-	for i in `seq 1 30`; do
-		set +e
-		psql -h localhost -U postgres -c '\conninfo' &>/dev/null
-		exitcode=$?
-		set -e
-		if [ $exitcode -eq 0 ]; then
-			echo "connection established to postgres"
-			return 0
-		fi
-		echo -n "."
-		sleep 1
-	done
-	echo "unable to connect to postgres"
-	exit 1
-}
+function bootDB {
+  db=$1
 
-function bootMysql {
-	echo -n "booting mysql"
-	(MYSQL_ROOT_PASSWORD=password  /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &
-	trycount=0
-	for i in `seq 1 30`; do
-		set +e
-		echo '\s;' | mysql -h 127.0.0.1 -u root --password="password" &>/dev/null
-		exitcode=$?
-		set -e
-		if [ $exitcode -eq 0 ]; then
-			echo "connection established to mysql"
-			return 0
-		fi
-		echo -n "."
-		sleep 1
-	done
-	echo "unable to connect to mysql"
-	exit 1
+  if [ "$db" = "postgres" ]; then
+    launchDB="(/docker-entrypoint.sh postgres &> /var/log/postgres-boot.log) &"
+    testConnection="psql -h localhost -U postgres -c '\conninfo' &>/dev/null"
+  elif [ "$db" = "mysql" ]; then
+    launchDB="(MYSQL_ROOT_PASSWORD=password /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
+    testConnection="echo '\s;' | mysql -h 127.0.0.1 -u root --password='password' &>/dev/null"
+  else
+    echo "skipping database"
+    return 0
+  fi
+
+  echo -n "booting $db"
+  eval "$launchDB"
+  trycount=0
+  for i in `seq 1 30`; do
+    set +e
+    eval "$testConnection"
+    exitcode=$?
+    set -e
+    if [ $exitcode -eq 0 ]; then
+      echo "connection established to $db"
+      return 0
+    fi
+    echo -n "."
+    sleep 1
+  done
+  echo "unable to connect to $db"
+  exit 1
 }
 
 go install ./vendor/github.com/onsi/ginkgo/ginkgo
 
+bootDB $DB
 
-if [ "${1:-""}" = "" ]; then
-  extraArgs=""
-else
-  extraArgs="${@}"
-fi
-
-
-if [ ${DB:-"none"} = "mysql" ]; then
-  bootMysql
-elif [ ${DB:-"none"} = "postgres" ]; then
-  bootPostgres
-else
-  echo "skipping database"
-fi
-
-ginkgo -r -p --race -randomizeAllSpecs -randomizeSuites -ldflags="-extldflags=-Wl,--allow-multiple-definition" ${extraArgs}
+ginkgo -r -p --race -randomizeAllSpecs -randomizeSuites \
+  -ldflags="-extldflags=-Wl,--allow-multiple-definition" \
+  ${@}
