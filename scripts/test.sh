cbe793aa0619abb40abd09fd07138384a9b2ad38
Amin Jamali <ajamali@pivotal.io>
2021-08-25 14:48:33 +0000

    ensure tests can run in CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,42 +5,11 @@ set -o pipefail
 
 cd $(dirname $0)/..
 
-function bootDB {
-  db=$1
+BIN_DIR="${PWD}/bin"
+mkdir -p "${BIN_DIR}"
+export PATH="${PATH}:${BIN_DIR}"
 
-  if [ "$db" = "postgres" ]; then
-    launchDB="(/docker-entrypoint.sh postgres &> /var/log/postgres-boot.log) &"
-    testConnection="psql -h localhost -U postgres -c '\conninfo' &>/dev/null"
-  elif [ "$db" = "mysql" ]  || [ "$db" = "mysql-5.6" ]; then
-    launchDB="(MYSQL_ROOT_PASSWORD=password /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
-    testConnection="echo '\s;' | mysql -h 127.0.0.1 -u root --password='password' &>/dev/null"
-  else
-    echo "skipping database"
-    return 0
-  fi
-
-  echo -n "booting $db"
-  eval "$launchDB"
-  trycount=0
-  for i in `seq 1 30`; do
-    set +e
-    eval "$testConnection"
-    exitcode=$?
-    set -e
-    if [ $exitcode -eq 0 ]; then
-      echo "connection established to $db"
-      return 0
-    fi
-    echo -n "."
-    sleep 1
-  done
-  echo "unable to connect to $db"
-  exit 1
-}
-
-go install ./vendor/github.com/onsi/ginkgo/ginkgo
-
-bootDB $DB
+go build -o "$BIN_DIR/ginkgo" github.com/onsi/ginkgo/ginkgo
 
 ginkgo -r -p --race -randomizeAllSpecs -randomizeSuites \
   -ldflags="-extldflags=-Wl,--allow-multiple-definition" \
